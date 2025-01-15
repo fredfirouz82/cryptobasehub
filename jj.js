d89e4361b6efddcf27964f727571e30345c7ccb1
fredfirouz82 <fredfirouz82@gmail.com>
2025-01-15 18:56:05 +1100

    Add files via upload

---
+++ jj.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for your message! We will get back to you soon.');
+    // Here you would typically send data to the server, but for this example, we'll just show an alert.
+});
